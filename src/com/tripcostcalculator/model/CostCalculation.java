5ce97baaff3ab2816a890e9a31d23bb41abf0fae
Taylor Smock <tsmock@vt.edu>
2013-03-08 09:43:12 -0500

    I've abstracted out some methods that will be used by other classes in Route/Toll, and added the beginnings of a cost-calculation method

---
+++ src/com/tripcostcalculator/model/CostCalculation.java
@@ -0,0 +1,16 @@
+package com.tripcostcalculator.model;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author vorpalblade
+ *  @version Mar 8, 2013
+ */
+
+public class CostCalculation
+{
+
+}
---
+++ src/com/tripcostcalculator/model/Route.java
@@ -0,0 +1,14 @@
+package com.tripcostcalculator.model;
+
+// -------------------------------------------------------------------------
+/**
+ *  Superclass for all potential route types
+ *
+ *  @author Taylor Smock (tsmock)
+ *  @version Mar 8, 2013
+ */
+
+public abstract class Route
+{
+    //Do nothing. This is just a super class
+}
