16d0d66b41dea601ace6104a2c7b1f102b1954b0
Taylor Smock <tsmock@vt.edu>
2013-03-07 20:57:14 -0500

    The VehicleTest class

---
+++ src/com/tripcostcalculator/model/VehicleTest.java
@@ -0,0 +1,69 @@
+package com.tripcostcalculator.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+// -------------------------------------------------------------------------
+/**
+ *  Testing for the vehicle class
+ *
+ *  @author Taylor Smock (tsmock)
+ *  @version Mar 7, 2013
+ */
+
+public class VehicleTest
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp()
+        throws Exception
+    {
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Vehicle#Vehicle(java.lang.String, float)}.
+     */
+    @Test
+    public void testVehicle()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Vehicle#getVehicleName()}.
+     */
+    @Test
+    public void testGetVehicleName()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Vehicle#getMpg()}.
+     */
+    @Test
+    public void testGetMpg()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Vehicle#setMpg(float)}.
+     */
+    @Test
+    public void testSetMpg()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
