1d4791aef49c6f5b0fb3a21b4dcab5edaaebf7e7
Taylor Smock <tsmock@vt.edu>
2013-03-07 20:55:52 -0500

    The MapTest class

---
+++ src/com/tripcostcalculator/model/MapTest.java
@@ -0,0 +1,81 @@
+package com.tripcostcalculator.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author vorpalblade
+ *  @version Mar 7, 2013
+ */
+
+public class MapTest
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp()
+        throws Exception
+    {
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Map#Map(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public void testMap()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Map#setStart(java.lang.String)}.
+     */
+    @Test
+    public void testSetStart()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Map#getStart()}.
+     */
+    @Test
+    public void testGetStart()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Map#setEnd(java.lang.String)}.
+     */
+    @Test
+    public void testSetEnd()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+
+    /**
+     * Test method for {@link com.tripcostcalculator.model.Map#getEnd()}.
+     */
+    @Test
+    public void testGetEnd()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
