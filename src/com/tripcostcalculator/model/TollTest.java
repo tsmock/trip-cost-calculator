be2944d9b42ed5d27b6c0bdddb1febdc82ee126f
Taylor Smock <tsmock@vt.edu>
2013-03-07 21:31:16 -0500

    We have toll objects and basic tests

---
+++ src/com/tripcostcalculator/model/TollTest.java
@@ -0,0 +1,38 @@
+package com.tripcostcalculator.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author vorpalblade
+ *  @version Mar 7, 2013
+ */
+
+public class TollTest
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp()
+        throws Exception
+    {
+    }
+
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
