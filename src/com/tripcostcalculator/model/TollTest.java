29206f93bac29e2ff89d23777e3f23f755140c31
Taylor Smock <tsmock@vt.edu>
2013-03-08 10:34:12 -0500

    Changing to GPLv3 for now. Will probably change later

--- src/com/tripcostcalculator/model/TollTest.java
+++ src/com/tripcostcalculator/model/TollTest.java
@@ -10,6 +10,22 @@ import org.junit.Test;
  *
  *  @author Taylor Smock (tsmock)
  *  @version Mar 7, 2013
+ *
+ *  This file is part of the Trip Cost Calculator.
+ *
+ *  Trip Cost Calculator is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Trip Cost Calculator is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Trip Cost Calculator.  If not, see
+ *  <http://www.gnu.org/licenses/>.
  */
 
 public class TollTest
