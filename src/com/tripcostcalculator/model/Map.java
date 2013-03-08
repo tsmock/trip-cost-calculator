e65d831650137215da95c75f761736d2e5bb5d7f
Taylor Smock <tsmock@vt.edu>
2013-03-07 20:48:59 -0500

    The beginnings of the 'map' class. Meant as a skeleton

---
+++ src/com/tripcostcalculator/model/Map.java
@@ -0,0 +1,21 @@
+package com.tripcostcalculator.model;
+
+// -------------------------------------------------------------------------
+/**
+ *  We need to get the maps here
+ *
+ *  @author Taylor Smock (tsmock)
+ *  @version Mar 7, 2013
+ */
+
+public class Map
+{
+    /**
+     * @param begin The beginning address
+     * @param end The ending address
+     */
+    public Map (String begin, String end)
+    {
+        //TODO figure out Maps API
+    }
+}
