944a834b89353a0f703988b58c04d2c1b6c054a6
Gareth Griffith <garethhgriffith@gmail.com>
2013-03-31 17:38:15 -0400

    updated MainActivity.java

--- src/com/tripcostcalculator/view/MainActivity.java
+++ src/com/tripcostcalculator/view/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tripcostcalculator.view;
 
+import sofia.app.Screen;
+
 import com.tripcostcalculator.R;
 import com.tripcostcalculator.R.layout;
 import com.tripcostcalculator.R.menu;
@@ -9,29 +11,13 @@ import android.view.Menu;
 
 // -------------------------------------------------------------------------
 /**
- *  Starts the android application
- *
- *  @author Taylor Smock (tsmock)
- *  @version Mar 7, 2013
+ * Starts the android application
+ * 
+ * @author Taylor Smock (tsmock)
+ * @author Gareth Griffith (gareth00)
+ * @version Mar 7, 2013
  */
-public class MainActivity
-    extends Activity
+public class MainActivity extends Screen 
 {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
 }
