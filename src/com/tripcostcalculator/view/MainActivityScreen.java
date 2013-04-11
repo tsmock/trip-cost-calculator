0b6903f9fd8e543321d5f7fb2ca109b053e28d05
Gareth Griffith <garethhgriffith@gmail.com>
2013-04-11 18:06:12 -0400

    Added various .xml screens

---
+++ src/com/tripcostcalculator/view/MainActivityScreen.java
@@ -0,0 +1,33 @@
+package com.tripcostcalculator.view;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Gareth Griffith
+ *  @version Apr 8, 2013
+ */
+
+public class MainActivityScreen
+{
+
+
+    public void initialize()
+    {
+
+
+    }
+
+    public void defineCarClicked()
+    {
+        //go to the car setup page
+    }
+
+    public void calcTripClicked()
+    {
+        //go to the calc trip page
+    }
+
+}
---
+++ src/com/tripcostcalculator/view/VehicleScreen.java
@@ -0,0 +1,30 @@
+package com.tripcostcalculator.view;
+
+import android.widget.EditText;
+import sofia.app.Screen;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Gareth Griffith (gareth00)
+ *  @version Apr 8, 2013
+ */
+
+public class VehicleScreen extends Screen
+{
+
+    public void initialize()
+    {
+
+    }
+
+
+
+    public void autoOkClicked()
+    {
+        //store values and go to the trip page
+    }
+
+}
