e3f9ccd5180c8f36b19f9cacd46aee740f5bb84b
Gareth Griffith <garethhgriffith@gmail.com>
2013-04-18 18:42:40 -0400

    trying to fix screen nav

--- src/com/tripcostcalculator/view/MainActivityScreen.java
+++ src/com/tripcostcalculator/view/MainActivityScreen.java
@@ -1,5 +1,8 @@
 package com.tripcostcalculator.view;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 
 // -------------------------------------------------------------------------
@@ -10,9 +13,10 @@ import android.widget.Button;
  *
  *  @author Gareth Griffith
  *  @version Apr 8, 2013
+ *  @param <E>
  */
 
-public class MainActivityScreen
+public class MainActivityScreen<E> extends Activity
 {
     private Button calcTrip;
     private Button defineCar;
@@ -27,6 +31,15 @@ public class MainActivityScreen
     public void defineCarClicked()
     {
         //go to the car setup page
+        //presentScreen(AutoScreen.class, auto);
+        startActivity(new Intent(this, AutoScreen.class));
+
+    }
+
+    public void defineCar1(View view)
+    {
+        startActivity(new Intent(this, AutoScreen.class));
+
     }
 
     public void calcTripClicked()
