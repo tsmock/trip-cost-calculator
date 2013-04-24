90eec09c80ca097b3c18c44f1e70658a25dfb74c
Taylor Smock <tsmock@vt.edu>
2013-04-24 10:43:57 -0400

    Started finishing up the information gathering classes.
    Still need to come up with a list that I can give the GUI.

--- src/com/tripcostcalculator/model/VehicleInformation.java
+++ src/com/tripcostcalculator/model/VehicleInformation.java
@@ -1,38 +1,42 @@
 package com.tripcostcalculator.model;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
-import java.io.InputStream;
 import com.tripcostcalculator.information.WebsiteInformation;
 
 // -------------------------------------------------------------------------
 /**
- *  We retrieve data from a website and interpret it.
+ * We retrieve data from a website and interpret it.
  *
- *  @author Taylor Smock (tsmock)
- *  @version Apr 4, 2013
+ * @author Taylor Smock (tsmock)
+ * @version Apr 4, 2013
  */
 
 public class VehicleInformation
 {
     private WebsiteInformation info;
     private WebsiteInformation manufacturer;
-    private String manufacturerString;
+    private String             manufacturerString;
     private WebsiteInformation makeModel;
-    private String makeModelString;
+    private String             makeModelString;
     private WebsiteInformation vehicle;
-    private String vehicleString;
+    private String             vehicleString;
+
 
     /**
      * We initialize vehicle information.
      */
     public VehicleInformation()
     {
-        info = new WebsiteInformation(
-            "http://www.fueleconomy.gov/mpg/MPG.do?action=browseList",
-            "manufacturers.info");
+        info =
+            new WebsiteInformation(
+                "http://www.fueleconomy.gov/mpg/MPG.do?action=browseList",
+                "manufacturers.info");
         info.saveFile();
     }
+
 
     // ----------------------------------------------------------
     /**
@@ -43,24 +47,20 @@ public class VehicleInformation
         return manufacturerString;
     }
 
+
     // ----------------------------------------------------------
     /**
-     * @param manufacturerString the manufacturerString to set
+     * @param manufacturerString
+     *            the manufacturerString to set
      */
     public void setManufacturerString(String manufacturerString)
     {
         this.manufacturerString = manufacturerString;
-        try
-        {
-            FileInputStream stream = new FileInputStream("manufacturers.info");
-            //stream.read(); //TODO finish this, copy logic to other classes or
-            //abstract out (probably abstract out to a separate method)
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
+        String addURL = "&make=" + this.manufacturerString;
+        this.manufacturer =
+            new WebsiteInformation(info.getWebsiteURL() + addURL, "model.info");
     }
+
 
     // ----------------------------------------------------------
     /**
@@ -71,15 +71,23 @@ public class VehicleInformation
         return makeModelString;
     }
 
+
     // ----------------------------------------------------------
     /**
-     * @param makeModelString the makeModelString to set
+     * @param makeModelString
+     *            the makeModelString to set
      */
     public void setMakeModelString(String makeModelString)
     {
         this.makeModelString = makeModelString;
-        //add logic to get next web page
+        String addURL = "&model=" + makeModelString;
+        this.makeModel =
+            new WebsiteInformation(
+                manufacturer.getWebsiteURL() + addURL,
+                "vehicle.info");
+        // add logic to get next web page
     }
+
 
     // ----------------------------------------------------------
     /**
@@ -90,17 +98,79 @@ public class VehicleInformation
         return vehicleString;
     }
 
+
     // ----------------------------------------------------------
     /**
-     * @param vehicleString the vehicleString to set
+     * @param vehicleString
+     *            the vehicleString to set
      */
     public void setVehicleString(String vehicleString)
     {
         this.vehicleString = vehicleString;
-        //TODO add logic to get the next web page/set of information
+        // TODO add logic to get the next web page/set of information
     }
 
 
+    // ----------------------------------------------------------
+    /**
+     * @return the manufacturer
+     */
+    public WebsiteInformation getManufacturer()
+    {
+        return manufacturer;
+    }
 
+
+    // ----------------------------------------------------------
+    /**
+     * @param manufacturer
+     *            the manufacturer to set
+     */
+    public void setManufacturer(WebsiteInformation manufacturer)
+    {
+        this.manufacturer = manufacturer; // TODO fill in info automagically
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * @return the makeModel
+     */
+    public WebsiteInformation getMakeModel()
+    {
+        return makeModel;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * @param makeModel
+     *            the makeModel to set
+     */
+    public void setMakeModel(WebsiteInformation makeModel)
+    {
+        this.makeModel = makeModel;// TODO fill in info automagically
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * @return the vehicle
+     */
+    public WebsiteInformation getVehicle()
+    {
+        return vehicle;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * @param vehicle
+     *            the vehicle to set
+     */
+    public void setVehicle(WebsiteInformation vehicle)
+    {
+        this.vehicle = vehicle; // TODO fill in info automagically
+    }
 
 }
