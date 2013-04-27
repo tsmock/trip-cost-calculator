646f07869fd32348639bdf9a7214455455d9f612
Gareth Griffith <garethhgriffith@gmail.com>
2013-04-27 19:32:47 -0400

    updates to .xml files, fixed problems with manifest.....we need a new API key (check manifest.xml) Map now shows on mapscreen

--- src/com/tripcostcalculator/view/MapScreen.java
+++ src/com/tripcostcalculator/view/MapScreen.java
@@ -1,7 +1,19 @@
 package com.tripcostcalculator.view;
 
+import android.support.v4.app.FragmentActivity;
+import com.tripcostcalculator.R;
 import android.widget.TextView;
-import sofia.app.Screen;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 // -------------------------------------------------------------------------
 /**
@@ -13,24 +25,72 @@ import sofia.app.Screen;
  *  @version Apr 14, 2013
  */
 
-public class MapScreen extends Screen
+public class MapScreen extends FragmentActivity
 {
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
     private TextView publicTransit;
     private TextView yourVehicle;
     private TextView vehicleCost;
     private TextView publicCost;
 
-    // ----------------------------------------------------------
-    /**
-     * We initialize the map screen.
-     */
-    public void initialize()
-    {
-        publicTransit.setText("Public Transit");
-        yourVehicle.setText("");
-        vehicleCost.setText("");
-        publicCost.setText("");
+//    // ----------------------------------------------------------
+//    /**
+//     * We initialize the map screen.
+//     */
+//    public void initialize()
+//    {
+//        publicTransit.setText("Public Transit");
+//        yourVehicle.setText("");
+//        vehicleCost.setText("");
+//        publicCost.setText("");
+//
+//    }
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.mapscreen);
+
+      publicTransit = (TextView)findViewById(R.id.publicTransit);
+      publicTransit.setText("Public Transit");
+      yourVehicle = (TextView)findViewById(R.id.yourVehicle);
+      yourVehicle.setText("Your Vehicle");
+      vehicleCost = (TextView)findViewById(R.id.vehicleCost);
+      vehicleCost.setText("");
+      publicCost = (TextView)findViewById(R.id.publicCost);
+      publicCost.setText("");
     }
 
+
+//      map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+//          .getMap();
+//      Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+//          .title("Hamburg"));
+//      Marker kiel = map.addMarker(new MarkerOptions()
+//          .position(KIEL)
+//          .title("Kiel")
+//          .snippet("Kiel is cool")
+//          .icon(BitmapDescriptorFactory
+//              .fromResource(R.drawable.ic_launcher)));
+//
+//      // Move the camera instantly to hamburg with a zoom of 15.
+//      map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+//
+//      // Zoom in, animating the camera.
+//      map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+//    }
+//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+    }
+
+
+    //TODO Bring in the MapFragment object from Google Maps
 }
