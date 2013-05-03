1c93b90e515afaf0d169eb5296b62be1b1b4cc0d
Gareth Griffith <garethhgriffith@gmail.com>
2013-05-03 16:19:17 -0400

    AutoScreenTest started

---
+++ src/com/tripcostcalculator/view/AutoScreenTest.java
@@ -0,0 +1,49 @@
+package com.tripcostcalculator.view;
+
+//import junit.framework.TestCase;
+
+public class AutoScreenTest
+    extends student.AndroidTestCase<AutoScreen>
+{
+
+    public AutoScreenTest()
+    {
+        super(AutoScreen.class);
+    }
+
+    protected void setUp()
+    {
+       // super.setUp();
+    }
+
+
+    public void testAutoOkClicked()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    public void testAutoMakeEditingDone()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    public void testAutoModelEditingDone()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    public void testAutoYearEditingDone()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    public void testAutoMPGEditingDone()
+    {
+        fail("Not yet implemented");
+    }
+
+}
