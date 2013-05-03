864d2d24b4696408604dddfa7b8f4c3bebba2bf4
Gareth Griffith <garethhgriffith@gmail.com>
2013-05-03 17:56:53 -0400

    Writing tests for TripScreen

--- src/com/tripcostcalculator/view/TripScreenTest.java
+++ src/com/tripcostcalculator/view/TripScreenTest.java
@@ -1,62 +1,62 @@
 package com.tripcostcalculator.view;
 
-import junit.framework.TestCase;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 // -------------------------------------------------------------------------
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
- * 
- *  @author vorpalblade
- *  @version May 3, 2013
+ * Tests all of the methods within the TripScreen Class
+ *
+ * @author Gareth Griffith (gareth00)
+ * @version May 3, 2013
  */
 
 public class TripScreenTest
-    extends TestCase
+    extends student.AndroidTestCase<TripScreen>
 {
+    private EditText startLoc;
+    private EditText endLoc;
+    private EditText gasPrice;
+    private TextView autoCost;
+    private TextView publicCost;
+    private Button   tripOkButton;
+
 
     // ----------------------------------------------------------
-    protected void setUp()
-        throws Exception
+    /**
+     * Create a new TripScreenTest object.
+     */
+    public TripScreenTest()
     {
-        super.setUp();
+        super(TripScreen.class);
+
+    }
+
+
+    // ----------------------------------------------------------
+    public void setUp()
+    {
+        // nothing to set up
     }
 
 
     /**
-     * Test method for {@link com.tripcostcalculator.view.TripScreen#tripOkButtonClicked()}.
+     * Test method to make sure that all of the EditText fields have been filled
+     * out and that the tripOk button has been enabled.
      */
     public void testTripOkButtonClicked()
     {
-        fail("Not yet implemented"); // TODO
+        enterText(startLoc, "here");
+        enterText(endLoc, "there");
+        enterText(gasPrice, "3.00");
+        click(tripOkButton);
     }
 
-
-    /**
-     * Test method for {@link com.tripcostcalculator.view.TripScreen#endLocEditingDone()}.
-     */
-    public void testEndLocEditingDone()
-    {
-        fail("Not yet implemented"); // TODO
-    }
-
-
-    /**
-     * Test method for {@link com.tripcostcalculator.view.TripScreen#startLocEditingDone()}.
-     */
-    public void testStartLocEditingDone()
-    {
-        fail("Not yet implemented"); // TODO
-    }
-
-
-    /**
-     * Test method for {@link com.tripcostcalculator.view.TripScreen#gasPriceEditingDone()}.
-     */
-    public void testGasPriceEditingDone()
-    {
-        fail("Not yet implemented"); // TODO
-    }
+// public void testAutoCostOnMapScreen()
+// {
+// assertEquals("2", autoCost.getText());
+// assertEquals("2", publicCost.getText());
+// }
 
 }
